5f87cd0720be39f23998fc0f7ba1a8075169b094
djShrek <boho527@gmail.com>
2014-12-04 15:58:59 -0800

    added and pushed migrations to sqlite3

---
+++ test/test_helper.rb
@@ -0,0 +1,11 @@
+require "bundler"
+
+Bundler.require
+Bundler.require :test
+
+ENV["APP_ENV"] = "test"
+
+require "config"
+
+DatabaseCleaner.strategy = :transaction
+
---
+++ test/unit/bootstrap_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BootstrapTest < MiniTest::Test
+  def test_bootstraps_test_suit
+    assert true
+  end
+end
