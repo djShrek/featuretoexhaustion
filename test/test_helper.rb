eeae4460ccc8570d19c2ee6a61d130f55e9564ae
djShrek <boho527@gmail.com>
2014-12-05 18:03:41 -0800

    included factories and tests helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,12 @@ Bundler.require :test
 ENV["APP_ENV"] = "test"
 
 require "config"
+require "factories"
+
 
 DatabaseCleaner.strategy = :transaction
+
+class Minitest::Test
+  include FactoryGirl::Syntax::Methods
+end
 
