e91ad121073b402df7d6bf3f5098b97fec91c57e
djShrek <boho527@gmail.com>
2014-12-08 16:29:25 -0800

    added test that make sure data pushed in is persisted

--- test/unit/book_hotel_test.rb
+++ test/unit/book_hotel_test.rb
@@ -3,9 +3,9 @@ require "away/services/book_hotel"
 
 module Away
   class BookHotelTest < MiniTest::Test
-    def test_books_an_hotel_room
-      # setup
-      @hotel = create(:hotel_with_extras, name: "Sheraton NY")
+
+    def setup 
+    @hotel = create(:hotel_with_extras, name: "Sheraton NY")
       @extras = @hotel.extras
       @data = {
         name: "Jose Mota",
@@ -17,14 +17,32 @@ module Away
       }
       @date = Time.new(2014, 12, 31)
 
-      # operation
+      @service = BookHotel.new(@hotel, @extras, @data, @date)
+      @service.execute
 
-      service = BookHotel.new(@hotel, @extras, @data, @date)
-      service.execute
-
-      # assertions
-
-      assert_kind_of Booking, service.booking
     end
+
+    def test_books_an_hotel_room
+      
+
+      assert_kind_of Booking, @service.booking
+    end
+
+    def test_shows_the_correct_information 
+      assert_equal "Jose Mota", @service.booking.name 
+    end
+
+    def test_refers_the_correct_hotel
+      assert_equal "Sheraton NY", @service.booking.hotel.name
+    end
+
+    def test_refers_to_the_correct_extras
+      assert_includes @service.booking.extras.map(&:name), "My Extra"
+    end
+
+    def test_shows_the_date
+      assert_equal @date, @service.booking.starts_at
+    end
+
   end
 end
